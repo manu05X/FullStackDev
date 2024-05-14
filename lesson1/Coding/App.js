b92eb2fbc18701e9b0bb71210d940b85f5ef1a41
manu05X <k.manu00005@gmail.com>
2024-05-14 12:50:10 +0530

    Using React.createElement create h1 object and nested elements

---
+++ lesson1/Coding/App.js
@@ -0,0 +1,29 @@
+/*
+const heading = React.createElement(
+    "h1",
+    {id:"heading", xyz:"abc"},
+    "Hello world from React!"
+);
+console.log(heading); //h1 in not html element but an object. Here heading is a object created by React.createElement and the root.render(heading) method convert this into html 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+*/
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},[
+        React.createElement("div", {id:"child1"},[
+            React.createElement("h1", {},"I'm an h1 tag"),
+            React.createElement("h2", {},"I'm an h2 tag"),
+        ]),
+        React.createElement("div", {id:"child2"},[
+            React.createElement("h1", {},"I'm an h1 tag"),
+            React.createElement("h2", {},"I'm an h2 tag"),
+        ])
+    ]
+);
+console.log(parent); //h1 in not html element but an object. Here heading is a object created by React.createElement and the root.render(heading) method convert this into html 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
