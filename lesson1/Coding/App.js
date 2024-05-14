734d0d41f3657f6f3d7c8f4d1d4227588b104d3f
manu05X <k.manu00005@gmail.com>
2024-05-14 17:43:37 +0530

    Adding the Name and then replacing it with react component

--- lesson1/Coding/App.js
+++ lesson1/Coding/App.js
@@ -1,4 +1,4 @@
-React.createElement("tagName",{Obj},"Content to be put in side h1");//-> syntax
+//React.createElement("tagName",{Obj},"Content to be put in side h1");//-> syntax
 
 /*
 
@@ -13,20 +13,31 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 */
 
+/*
+<div id="root">
+    <h1 id="title" class="title" style="background: red;">heading</h1>
+</div>
+
+*/
 const parent = React.createElement(
     "div",
     {id:"parent"},[
-        React.createElement("div", {id:"child1"},[
-            React.createElement("h1", {},"I'm an h1 tag"),
-            React.createElement("h2", {},"I'm an h2 tag"),
+        React.createElement("div",{id:"children1"},[
+            React.createElement("h1",{},"I am h1 tag"),
+            React.createElement("h2",{},"I am h2 tag")
         ]),
-        React.createElement("div", {id:"child2"},[
-            React.createElement("h1", {},"I'm an h1 tag"),
-            React.createElement("h2", {},"I'm an h2 tag"),
+        React.createElement("div",{id:"children2"},[
+            React.createElement("h1",{},"I am h1 tag"),
+            React.createElement("h2",{},"I am h2 tag")
         ])
     ]
 );
+  
+
 console.log(parent); //h1 in not html element but an object. Here heading is a object created by React.createElement and the root.render(heading) method convert this into html 
+
+//create root using createRoot
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+//pass react element inside root
 root.render(parent);
