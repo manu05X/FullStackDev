5f6f068377eb718b74bf547833ff4000052c1f3f
manu05X <k.manu00005@gmail.com>
2024-05-18 01:22:20 +0530

    Using the Hook and the state variable of react to implement filter

--- lesson4/Coding/src/components/Body.js
+++ lesson4/Coding/src/components/Body.js
@@ -1,26 +1,35 @@
 import ResturantCards from "./ResturantCards";
 import { restaurantList } from "../utils/mockData";
+import { useState } from "react";
 
-
-// const Body = () => {
-//     return(
-//         <div className="body">
-//             <div className="Search">Search</div>
-//             <div className="resturant-Container">
-//                 <ResturantCards/>
-                
-//             </div>
-            
-//         </div>
-//     )
-// }
 
 const Body = () => {
+    //State Variables
+    //Whenever a state variable is updated react will re-render my component
+    const [listOfRestaurants, setListOfResturants] = useState(restaurantList);
+    //Normal Js Variable
+    //let listOfRestaurants;
+
+
+
     return (
         <div className="body">
-            <div className="Search">Search</div>
+            <div className="filter">
+                <button 
+                    className="filter-btn" 
+                    onClick={()=>{
+                        const filteredListOfResturants = listOfRestaurants.filter( 
+                                (res) => res.info.avgRating > 4.3
+                            );
+                            // console.log(listOfRestaurants);
+                            setListOfResturants(filteredListOfResturants);
+                        }}
+                    >
+                    Top Rated Resturant
+                </button>
+            </div>
             <div className="resturant-Container">
-                {restaurantList.map((restaurantData, index) => (
+                {listOfRestaurants.map((restaurantData, index) => (
                     <ResturantCards key={index} resData={restaurantData.info} />
                 ))}
             </div>
