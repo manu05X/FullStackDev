334ea700bb3a5193e29ac010bc00c1a3946cc645
manu05X <k.manu00005@gmail.com>
2024-05-31 14:45:20 +0530

    Creating class component and how to use useState

--- lesson4/Coding/src/components/UserClass.js
+++ lesson4/Coding/src/components/UserClass.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { useState } from "react";
 
 
 class UserClass extends React.Component {
@@ -6,15 +7,30 @@ class UserClass extends React.Component {
     //define class constructor to recieve props with keyword super(props)
     constructor(props) {
         super(props);
+        //console.log(props);
 
-        console.log(props);
+        //state variable
+
+        this.state = {
+            count : 0,
+            count2 : 2,
+        };
     }
 
     render() {
+        const {name, location} = this.props; //destructure
+        const {count, count2} = this.state;
+        
+
+
         return(
             <div className="user-card">
-                <h2>Name : {this.props.name}</h2>
-                <h3>Location : Banglore</h3>
+                <h1> Count : {count}</h1>
+                <h1> Count2 : {count2}</h1>
+                {/* <h1>Count : {this.state.count}</h1> */}
+                {/* <h2>Name : {this.props.name}</h2> */}
+                <h2>Name : {name}</h2>
+                <h3>Location : {location}</h3>
                 <h4>Contact : @manish</h4>
             </div>
         )
