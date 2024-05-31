517ddbd1e67b32fe57df6d91b0a9f2a93a565e0f
manu05X <k.manu00005@gmail.com>
2024-05-31 16:33:02 +0530

    Made AboutClass component

---
+++ lesson4/Coding/src/components/AboutClass.js
@@ -0,0 +1,45 @@
+import {Component} from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+
+
+class AboutClass extends Component {
+
+    constructor(props) {
+        super(props);
+
+        console.log("AboutClass -> parent class Constructor called")
+    }
+
+    componentDidMount() {
+        console.log("Parent ComponentDidMounted called");
+    }
+
+    render() {
+        console.log("AboutClass -> parent Rendeder Method is called");
+        return (
+            <div>
+            <h1>About</h1>
+            <h2>This is Namaste React Web Series.</h2>
+            {/* <User name={"Manish (Functional Component)"}/> */}
+            <UserClass name={"Manish (class)"} location={"Banglore"}/>
+        </div>
+
+        )
+    };
+};
+
+export default AboutClass;
+
+// const About = () => {
+//     return (
+//         <div>
+//             <h1>About</h1>
+//             <h2>This is Namaste React Web Series.</h2>
+//             {/* <User name={"Manish (Functional Component)"}/> */}
+//             <UserClass name={"Manish (class)"} location={"Banglore"}/>
+//         </div>
+//     );
+// };
+
+// export default About;
