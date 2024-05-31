615018aecc52b2093c97f52fe3b9c2bcf658ece7
manu05X <k.manu00005@gmail.com>
2024-05-31 15:03:48 +0530

    how to update class component hook

--- lesson4/Coding/src/components/About.js
+++ lesson4/Coding/src/components/About.js
@@ -6,8 +6,8 @@ const About = () => {
         <div>
             <h1>About</h1>
             <h2>This is Namaste React Web Series.</h2>
-            <User name={"Manish (Functional Component)"}/>
-            <UserClass name={"Manish (class)"}/>
+            {/* <User name={"Manish (Functional Component)"}/> */}
+            <UserClass name={"Manish (class)"} location={"Banglore"}/>
         </div>
     );
 };
--- lesson4/Coding/src/components/UserClass.js
+++ lesson4/Coding/src/components/UserClass.js
@@ -13,7 +13,7 @@ class UserClass extends React.Component {
 
         this.state = {
             count : 0,
-            count2 : 2,
+            count2 : 0,
         };
     }
 
@@ -26,7 +26,17 @@ class UserClass extends React.Component {
         return(
             <div className="user-card">
                 <h1> Count : {count}</h1>
-                <h1> Count2 : {count2}</h1>
+                <h2>Count2 : {count2}</h2>
+                <button onClick={()=>{
+                    this.setState({
+                        count : this.state.count + 1,
+                        count2 : this.state.count2 + 1,
+                    });
+                    //console.log("render on click " + count)
+                }}
+                >
+                    Count Increase
+                </button>
                 {/* <h1>Count : {this.state.count}</h1> */}
                 {/* <h2>Name : {this.props.name}</h2> */}
                 <h2>Name : {name}</h2>
